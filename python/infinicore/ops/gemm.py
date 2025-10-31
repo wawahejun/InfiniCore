2119e6200a1112b438af45acfa46fce123ec2482
zhuyue <zhuyue@qiyuanlab.com>
2025-10-31 17:02:22 +0800

    Delete gemm python interface and tests.

--- python/infinicore/ops/gemm.py
+++
@@ -1,9 +0,0 @@
-from infinicore.lib import _infinicore
-from infinicore.tensor import Tensor
-
-
-def gemm(input, other, alpha=1.0, beta=0.0, *, out=None):
-    if out is None:
-        return Tensor(_infinicore.gemm(input._underlying, other._underlying, alpha, beta))
-
-    _infinicore.gemm_(out._underlying, input._underlying, other._underlying, alpha, beta)
